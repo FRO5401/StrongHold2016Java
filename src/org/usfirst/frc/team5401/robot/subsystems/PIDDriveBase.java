75b298021754f31c42fe4d741b96aeda6ee938e2
kjm16216 <kmckay@bensalemsd.org>
2016-11-16 19:56:57 -0500

    PIDDriveBase

---
+++ src/org/usfirst/frc/team5401/robot/subsystems/PIDDriveBase.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team5401.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+public class PIDDriveBase extends PIDSubsystem {
+
+	@Override
+	protected double returnPIDInput() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
