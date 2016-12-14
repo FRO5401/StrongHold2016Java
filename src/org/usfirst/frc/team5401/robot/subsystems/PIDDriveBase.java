949bef36a2d6a946f4f73e8fcfb79bd1a01b0804
ptoil <suhailmallick@gmail.com>
2016-12-14 00:14:22 -0500

    PID Drivebase
    still need to make use of PID output

--- src/org/usfirst/frc/team5401/robot/subsystems/PIDDriveBase.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/PIDDriveBase.java
@@ -1,25 +1,46 @@
 package org.usfirst.frc.team5401.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
-import 
+import edu.wpi.first.wpilibj.Victor;
+
+import org.usfirst.frc.team5401.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 public class PIDDriveBase extends PIDSubsystem {
-
+	
+	private Victor LeftDrive1;
+	private Victor LeftDrive2;
+	private Victor RightDrive1;
+	private Victor RightDrive2;
+	private ADXRS450_Gyro MainGyro;
+	
+	public PIDDriveBase () {
+		super("PIDDriveBase", 1.0, 0.0, 0.0);
+		setAbsoluteTolerance(.05);
+		getPIDController().setContinuous(false);
+		
+		LeftDrive1 = new Victor(RobotMap.LeftMotor1);
+		LeftDrive2 = new Victor(RobotMap.LeftMotor2);
+		RightDrive1 = new Victor(RobotMap.RightMotor1);
+		RightDrive2 = new Victor(RobotMap.RightMotor2);
+		
+		MainGyro = new ADXRS450_Gyro();
+		MainGyro.reset();
+	}
+	
 	@Override
 	protected double returnPIDInput() {
-		// TODO Auto-generated method stub
-		return 0;
+		return MainGyro.getAngle();
 	}
 
 	@Override
 	protected void usePIDOutput(double output) {
-		// TODO Auto-generated method stub
-
+		//use output for drive command
 	}
 
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
 
 	}
 
