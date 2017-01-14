52e2227a650e046fc89688332ff0ef8b508601b4
jxfireruby <jxfireruby@gmail.com>
2017-01-14 11:09:52 -0500

    Added Untested PIDXboxMove

---
+++ src/org/usfirst/frc/team5401/robot/commands/PIDXboxMove.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team5401.robot.commands;
+
+import edu.wpi.first.wpilibj.command.PIDCommand;
+
+public class PIDXboxMove extends PIDCommand {
+
+	@Override
+	protected double returnPIDInput() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
